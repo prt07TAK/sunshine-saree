3244a30b3d215284771b77bed86055707aa15eef
prt07TAK <takpulkit879@gmail.com>
2026-06-04 17:00:13 +0530

    update backend url

--- sunshine-saree/client/src/utils/api.js
+++ sunshine-saree/client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'https://sunshine-saree-backend.onrender.com/api',
 });
 
 // Add JWT token to requests
